aed73c97d7a2e74f17169c96735eae5e92de493a
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-02-23 09:55:57 -0500

    See #30.  Turned the SCHEMA_REGISTRY constants and FLINK constants to a dict.

--- src/cc_clients_lib/flink_sql_client.py
+++ src/cc_clients_lib/flink_sql_client.py
@@ -12,24 +12,26 @@ __status__     = "dev"
 
 
 # Flink SQL Config Keys.
-FLINK_API_KEY = "flink_api_key"
-FLINK_API_SECRET = "flink_api_secret"
-ORGANIZATION_ID="organization_id"
-ENVIRONMENT_ID="environment_id"
-CLOUD_PROVIDER="cloud_provider"
-CLOUD_REGION="cloud_region"
-COMPUTE_POOL_ID="compute_pool_id"
+FLINK_CONFIG = {
+    "flink_api_key": "flink_api_key",
+    "flink_api_secret": "flink_api_secret",
+    "organization_id": "organization_id",
+    "environment_id": "environment_id",
+    "cloud_provider": "cloud_provider",
+    "cloud_region": "cloud_region",
+    "compute_pool_id": "compute_pool_id"
+}
 
 
 class FlinkSqlClient():
     def __init__(self, flink_sql_config: dict):
-        self.organization_id = flink_sql_config[ORGANIZATION_ID]
-        self.environment_id = flink_sql_config[ENVIRONMENT_ID]
-        self.flink_api_key = str(flink_sql_config[FLINK_API_KEY])
-        self.flink_api_secret = str(flink_sql_config[FLINK_API_SECRET])
-        self.cloud_provider = flink_sql_config[CLOUD_PROVIDER]
-        self.cloud_region = flink_sql_config[CLOUD_REGION]
-        self.compute_pool_id = flink_sql_config[COMPUTE_POOL_ID]
+        self.organization_id = flink_sql_config[FLINK_CONFIG["organization_id"]]
+        self.environment_id = flink_sql_config[FLINK_CONFIG["environment_id"]]
+        self.flink_api_key = str(flink_sql_config[FLINK_CONFIG["flink_api_key"]])
+        self.flink_api_secret = str(flink_sql_config[FLINK_CONFIG["flink_api_secret"]])
+        self.cloud_provider = flink_sql_config[FLINK_CONFIG["cloud_provider"]]
+        self.cloud_region = flink_sql_config[FLINK_CONFIG["cloud_region"]]
+        self.compute_pool_id = flink_sql_config[FLINK_CONFIG["compute_pool_id"]]
         self.flink_sql_base_url = f"https://flink.${self.cloud_region}.${self.cloud_provider}.confluent.cloud/sql/v1/organizations/${self.organization_id}/environments/${self.environment_id}/"
 
     def delete_flink_statement(self, statement_name: str) -> Tuple[int, str]:

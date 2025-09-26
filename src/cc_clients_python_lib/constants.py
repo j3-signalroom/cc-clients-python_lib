eec903121af144f2d1e71075b7a3bd474b77378a
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-09-26 16:10:27 -0400

    Resolved #299.

---
+++ src/cc_clients_python_lib/constants.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+
+__copyright__  = "Copyright (c) 2025 Jeffrey Jonathan Jennings"
+__credits__    = ["Jeffrey Jonathan Jennings"]
+__license__    = "MIT"
+__maintainer__ = "Jeffrey Jonathan Jennings"
+__email__      = "j3@thej3.com"
+__status__     = "dev"
+
+
+DEFAULT_REQUEST_TIMEOUT_IN_SECONDS: Final[int] = 30
